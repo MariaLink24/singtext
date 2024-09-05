f4f6fb8f6624cedffc3886e07c78719509e7e28d
MariaLink24 <fromnarniawithlove@gmail.com>
2024-09-05 20:36:29 +0300

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import manifest from './manifest.json'
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({  manifest, includeAssets: ['pwa-192x192.svg'] })],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'sing-a-song',
+      short_name: 'sing-a-song',
+      description: 'sing-a-song',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
 })
